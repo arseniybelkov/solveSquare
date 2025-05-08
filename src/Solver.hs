bd3529980083143a56cc189f55a552162f77662f
Arseniy Belkov <arsfizteh@gmail.com>
2025-05-09 00:57:14 +0300

    cleaner code

--- src/Solver.hs
+++ src/Solver.hs
@@ -1,19 +1,23 @@
 module Solver (solveSquare) where
 
 
-data Roots = None | One Double | Two Double Double deriving (Show)
+data Roots = NoRoots | SingleRoot Double | TwoRoots Double Double deriving (Show)
+
+
+kEpsilon :: Double
+kEpsilon = 1e-10
 
 
 solveSquare :: Double -> Double -> Double -> Roots
 solveSquare a b c 
-    | abs a < 1e-10 = intoRoots (solveLinear b c)
+    | abs a < kEpsilon = maybe NoRoots SingleRoot (solveLinear b c)
     | otherwise = findQuadraticRoots a b c
 
 
 findQuadraticRoots :: Double -> Double -> Double -> Roots
 findQuadraticRoots a b c = case calculateDisc a b c of 
-    Just sqrt_d -> Two ((-b - sqrt_d ) / (2 * a)) ((-b + sqrt_d ) / (2 * a))
-    Nothing -> None
+    Just sqrt_d -> TwoRoots ((-b - sqrt_d ) / (2 * a)) ((-b + sqrt_d ) / (2 * a))
+    Nothing -> NoRoots 
 
 
 calculateDisc :: Double -> Double -> Double -> Maybe Double
@@ -21,15 +25,10 @@ calculateDisc a b c
     | disc >= 0.0 = Just (sqrt disc)
     | otherwise = Nothing
     where disc = b * b - 4 * a * c
-        
-
-intoRoots :: Maybe Double -> Roots
-intoRoots (Just x) = One x
-intoRoots Nothing = None
 
 
 solveLinear :: Double -> Double -> Maybe Double
 -- ax + b = 0
 solveLinear a b
-    | abs a < 1e-10 = Nothing
+    | abs a < kEpsilon = Nothing
     | otherwise = Just (-b / a)
