e6aff456cd216fee15b35ef9d93f0396e069aeaf
Arseniy Belkov <arsfizteh@gmail.com>
2025-05-12 23:39:04 +0300

    float and double generic draft

--- src/Solver.hs
+++ src/Solver.hs
@@ -4,34 +4,40 @@ module Solver (solveSquare) where
 data Roots = NoRoots | SingleRoot Double | TwoRoots Double Double deriving (Show)
 
 
-kEpsilon :: Double
+class FloatingPoint f where
+
+instance FloatingPoint Float where
+instance FloatingPoint Double where
+
+
+kEpsilon :: FloatingPoint f => f
 kEpsilon = 1e-10
 
 
-isZero :: Double -> Bool
+isZero :: FloatingPoint f => f -> Bool
 isZero x = abs x < kEpsilon
 
 
-solveSquare :: Double -> Double -> Double -> Roots
+solveSquare :: FloatingPoint f => f -> f -> f -> Roots
 solveSquare a b c 
     | isZero a = maybe NoRoots SingleRoot (solveLinear b c)
     | otherwise = findQuadraticRoots a b c
 
 
-findQuadraticRoots :: Double -> Double -> Double -> Roots
+findQuadraticRoots :: FloatingPoint f => f -> f -> f -> Roots
 findQuadraticRoots a b c = case calculateDisc a b c of 
     Just sqrt_d -> TwoRoots ((-b - sqrt_d ) / (2 * a)) ((-b + sqrt_d ) / (2 * a))
     Nothing -> NoRoots 
 
 
-calculateDisc :: Double -> Double -> Double -> Maybe Double
+calculateDisc :: FloatingPoint f => f -> f -> f -> Maybe f
 calculateDisc a b c
     | disc >= 0.0 = Just (sqrt disc)
     | otherwise = Nothing
     where disc = b * b - 4 * a * c
 
 
-solveLinear :: Double -> Double -> Maybe Double
+solveLinear :: FloatingPoint f => f -> f -> Maybe f
 -- ax + b = 0
 solveLinear a b
     | isZero a = Nothing
