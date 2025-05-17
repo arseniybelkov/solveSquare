56bf2870b1d0f079c74eaa00a26e7f19c0a2cead
Arseniy Belkov <arsfizteh@gmail.com>
2025-05-17 16:04:24 +0300

    equation can now be solved using single precision float

--- src/Solver.hs
+++ src/Solver.hs
@@ -1,10 +1,9 @@
 module Solver (solveSquare) where
 
 
-data Roots = NoRoots | SingleRoot Double | TwoRoots Double Double deriving (Show)
+data Roots f = NoRoots | SingleRoot f | TwoRoots f f deriving (Show)
 
-
-class FloatingPoint f where
+class (Floating f, Ord f, Fractional f) => FloatingPoint f where
 
 instance FloatingPoint Float where
 instance FloatingPoint Double where
@@ -18,13 +17,13 @@ isZero :: FloatingPoint f => f -> Bool
 isZero x = abs x < kEpsilon
 
 
-solveSquare :: FloatingPoint f => f -> f -> f -> Roots
+solveSquare :: FloatingPoint f => f -> f -> f -> Roots f
 solveSquare a b c 
     | isZero a = maybe NoRoots SingleRoot (solveLinear b c)
     | otherwise = findQuadraticRoots a b c
 
 
-findQuadraticRoots :: FloatingPoint f => f -> f -> f -> Roots
+findQuadraticRoots :: FloatingPoint f => f -> f -> f -> Roots f
 findQuadraticRoots a b c = case calculateDisc a b c of 
     Just sqrt_d -> TwoRoots ((-b - sqrt_d ) / (2 * a)) ((-b + sqrt_d ) / (2 * a))
     Nothing -> NoRoots 
